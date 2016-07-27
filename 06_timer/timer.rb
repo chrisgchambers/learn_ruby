feddc7550a69b8c426b5e94571463456f5b24ac4
Chris Chambers <chrisgchambers@gmail.com>
2016-07-27 16:59:08 -0600

    06_timer passed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,36 @@
 class Timer
-  #write your code here
+  def initialize()
+    @seconds = 0;
+  end
+
+  def seconds()
+    @seconds
+  end
+
+  def seconds=(value)
+    @seconds = value
+  end
+
+  def time_string()
+    distribute_seconds = self.seconds
+
+    # find the number of whole hours
+    hr = distribute_seconds / 3600
+    # pull out remaining seconds
+    distribute_seconds = distribute_seconds % 3600
+    # find the number of whole minutes
+    min = distribute_seconds / 60
+    # pull out remaining seconds
+    sec = distribute_seconds % 60
+    # return string with 1-digit results padded - convert fixnum using string interpolation
+    time_string = "#{padded(hr)}:#{padded(min)}:#{padded(sec)}"
+  end
+
+  def padded(value)
+    if "#{value}".length == 1
+      return "0#{value}"
+    else
+      return "#{value}"
+    end
+  end
 end
