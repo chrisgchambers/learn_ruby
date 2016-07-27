33fa59e9af5c3b87661c065b36d56bd27cc3b646
Chris Chambers <chrisgchambers@gmail.com>
2016-07-27 12:08:19 -0600

    01_temperature passed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+# make factors 2-decimal place to set precision past integer
+# 5/9 == 0 but 5.0/9 == 0.5555555555
+def ftoc (farrenheit)
+  return ((farrenheit - 32) * 5.00 / 9.00)
+end
+
+def ctof (celsius)
+  return (celsius * 9.00 / 5.00 + 32)
+end
