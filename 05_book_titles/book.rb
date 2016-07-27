3e0e1a2f3860cf4232a99af39a339269f407941a
Chris Chambers <chrisgchambers@gmail.com>
2016-07-27 16:08:15 -0600

    05_book_titles passed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  def title
+    @title
+  end
+  def title=(string_in)
+    words = string_in.split(' ')
+    words[0][0] = words[0][0].upcase
+    for word in words do
+      if !["the","and","in","on","over", "of", "a", "an"].include?(word)
+        word[0] = word[0].upcase
+      end
+    end
+    @title = words.join(" ")
+  end
 end
