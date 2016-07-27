4e76c1ca1f82f2e2bb1fdada0b126e785d95f404
Chris Chambers <chrisgchambers@gmail.com>
2016-07-27 12:39:03 -0600

    02_calculator passed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+def add (a, b)
+  return a + b
+end
+
+def subtract (a, b)
+  return a - b
+end
+
+def sum (a)
+  total = 0
+  a.each {|x| total+=x}
+  return total
+end
+
+def multiply (*p)
+  product = 1
+  p.each {|x| product *= x}
+  return product
+end
+
+def power (a,b)
+  result = 1
+  b.times do
+    result *= a
+  end
+  return result
+end
+
+def factorial (p)
+  product = 1
+  if p > 0
+    p.times do
+      product *= p
+      p -= 1
+    end
+  end
+  return product
+end
